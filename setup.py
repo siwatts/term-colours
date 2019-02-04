bd8a5c5121bbff6386b253109937022f6606e794
Simon Watts <siwatts15@outlook.com>
2019-02-04 22:04:34 +0000

    Setup inc. module structure, setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='term-colours',
+      version='0.1',
+      description='Convert between terminal colour schemes, and display them.',
+      url='https://github.com/siwatts/term-colours',
+      author='Simon Watts',
+      author_email='',
+      license='MIT',
+      packages=['term-colours'],
+      zip_safe=False)
+
